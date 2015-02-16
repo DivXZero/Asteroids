b633d3c32b50383f1421f13b56a1159a56e4706b
Nick <silv3rlogic@gmail.com>
2015-02-16 15:15:31 -0800

    Added OpenGL

--- source/main.cpp
+++ source/main.cpp
@@ -1,13 +1,16 @@
 
+#include <SFML/OpenGL.hpp>
 #include <SFML/Graphics.hpp>
 #include <Box2D/Box2D.h>
 
 int main(int argc, char** argv[])
 {
-	sf::Window window(sf::VideoMode(1024, 768), "Asteroids", sf::Style::Close);
+	sf::RenderWindow window(sf::VideoMode(1024, 768), "Asteroids", sf::Style::Close);
 
 	b2Vec2 gravity(0.0f, 9.8f);
 	b2World world(gravity);
+
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
 	sf::Event event;
 	while (window.isOpen())
@@ -24,6 +27,10 @@ int main(int argc, char** argv[])
 				break;
 			}
 		}
+
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		window.display();
 	}
 
 	return 0;
